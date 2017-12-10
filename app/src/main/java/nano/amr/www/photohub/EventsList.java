4b4af92ab625d768ac3eb8c9e17dbe1c4780faba
Amr AbuZant <amrabuzant@gmail.com>
2017-12-10 21:24:07 +0200

    Inital Setup

---
+++ app/src/main/java/nano/amr/www/photohub/EventsList.java
@@ -0,0 +1,13 @@
+package nano.amr.www.photohub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events_list);
+    }
+}
